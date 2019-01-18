6ac64c2eb576216bfa43d45a106c263dcd936f67
Gbhoot <gsbhoot1020@gmail.com>
2019-01-18 09:50:04 -0800

    Initial Commit

---
+++ historicTeamz/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  historicTeamz
+//
+//  Created by Gurpal Bhoot on 1/18/19.
+//  Copyright © 2019 Gurpal Bhoot. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
